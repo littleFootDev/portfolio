d49b6e312289a0623196493eaaa51c71fed651b8
LittleFootDev <thelittlefootdev@gmail.com>
2021-07-30 10:43:16 +0200

    html and css for home page + animation "

--- app.js
+++ app.js
@@ -21,3 +21,37 @@ if (window.matchMedia("max-width:1300px")) {
     });
   });
 }
+// Annimation écriture
+
+const txtAnim = document.querySelector(".txt-animation");
+
+let typewriter = new Typewriter(txtAnim, {
+  loop: false,
+  deleteSpeed: 20,
+});
+typewriter
+  .pauseFor(1800)
+  .changeDelay(20)
+  .typeString("Moi c'est Jacques Caradi")
+  .pauseFor(300)
+  .typeString("<strong>, Developpeur Full-Stack</strong> !")
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString('<span style="color: #27ae60"> M.E.R.N</span> !')
+  .pauseFor(1000)
+  .deleteChars(9)
+  .typeString('<span style="color: #EA39ff"> Javascript</span> !')
+  .pauseFor(1000)
+  .deleteChars(12)
+  .typeString('<span style="color: midnightblue"> Node.js</span> !')
+  .pauseFor(1000)
+  .deleteChars(9)
+  .typeString('<span style="color: #ff6910"> React</span> !')
+  .pauseFor(1000)
+  .deleteChars(7)
+  .typeString('<span style="color: red"> Swift</span> !')
+  .pauseFor(1000)
+  .deleteChars(7)
+  .typeString('<span style="color: purple"> C#</span> !')
+
+  .start();
